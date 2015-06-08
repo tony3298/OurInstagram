c95487cb9d4dcb5d9a8a0d6cc0fc93888eefea16
tony3298 <tonydakhoul@yahoo.com>
2015-06-08 15:24:37 -0500

    Initial Commit

---
+++ OurInstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OurInstagram
+//
+//  Created by Tony Dakhoul on 6/8/15.
+//  Copyright (c) 2015 Tony Dakhoul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OurInstagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OurInstagram
+//
+//  Created by Tony Dakhoul on 6/8/15.
+//  Copyright (c) 2015 Tony Dakhoul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
