ce8227f56c554b9769d304a257db7cf88b3abfe9
Bryon Finke <bryon.finke@gmail.com>
2015-06-10 12:36:19 -0500

    creates login viewcontroller

---
+++ OurInstagram/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  OurInstagram
+//
+//  Created by Bryon Finke on 6/10/15.
+//  Copyright (c) 2015 Tony Dakhoul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ OurInstagram/LoginViewController.m
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.m
+//  OurInstagram
+//
+//  Created by Bryon Finke on 6/10/15.
+//  Copyright (c) 2015 Tony Dakhoul. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
