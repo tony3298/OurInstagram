ccc67deca189a4c1c1d28e4f9adea6d2e1c919c7
tony3298 <tonydakhoul@yahoo.com>
2015-06-10 16:30:35 -0500

    Camera working and edit profile view controller added

---
+++ OurInstagram/CameraView.m
@@ -0,0 +1,21 @@
+//
+//  CameraView.m
+//  OurInstagram
+//
+//  Created by Tony Dakhoul on 6/10/15.
+//  Copyright (c) 2015 Tony Dakhoul. All rights reserved.
+//
+
+#import "CameraView.h"
+
+@implementation CameraView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ OurInstagram/EditProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditProfileViewController.h
+//  OurInstagram
+//
+//  Created by Tony Dakhoul on 6/10/15.
+//  Copyright (c) 2015 Tony Dakhoul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditProfileViewController : UIViewController
+
+@end
